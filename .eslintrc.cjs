776c36d63de15e61b54e40d4661179284331301c
Locon213 <167447571+Locon213@users.noreply.github.com>
2025-08-26 18:05:46 +0300

    chore: ignore build output for lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,14 @@ module.exports = {
     es2022: true,
     node: true,
   },
-  extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
